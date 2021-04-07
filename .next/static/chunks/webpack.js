6daa95c26744d8a61f9837c569bcbb3f2ae33814
Daniel Silva <dani3l.dxp@gmail.com>
2021-04-07 18:07:14 -0300

    chore: importando google fonts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bf6d18934b0d3b1595de";
+/******/ 	var hotCurrentHash = "e2194310445658f2b483";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
