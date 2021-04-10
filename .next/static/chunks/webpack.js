940648826f99bb17d22479e871a40e0a508e3c80
Daniel Silva <dani3l.dxp@gmail.com>
2021-04-10 08:15:31 -0300

    feat: API routes no Next.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "827b840448016f36b5f2";
+/******/ 	var hotCurrentHash = "3956993973947a54b4cb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
