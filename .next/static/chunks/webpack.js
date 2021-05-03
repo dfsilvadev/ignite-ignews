e5b338f127580612d1082e620285c0fd731790f4
Daniel Silva <dani3l.dxp@gmail.com>
2021-05-03 17:59:47 -0300

    chore: instalando, configurando e testando o typescript no projeto

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58c71c3cccf10c59223d"; }
+/******/ 		__webpack_require__.h = function() { return "c5ddac1e7902a5b8d744"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
