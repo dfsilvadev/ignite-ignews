90cb13e6cc5aebcbc7fc2429d44bcd33b3849ba1
Daniel Silva <dani3l.dxp@gmail.com>
2021-05-05 13:41:49 -0300

    feat: implementando o signIn button e o header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignInButton } from "../SigninButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Logotipo ignews" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
