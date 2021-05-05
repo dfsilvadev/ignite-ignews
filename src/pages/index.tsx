eb3b9d4a910b77ec1ebaa1295264dbedc48eeadc
Daniel Silva <dani3l.dxp@gmail.com>
2021-05-05 15:20:14 -0300

    estilizando a tela home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Ignews</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img
+          src="/images/avatar.svg"
+          alt="Girl with a notebook in front of developing "
+        />
+      </main>
     </>
   );
 }
