d09f614b8f888ef54b827758f76a1acbf823ebbf
Daniel Silva <dani3l.dxp@gmail.com>
2021-05-24 18:05:28 -0300

    feat: criando o subscribe button

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export function SubscriberButton() {
+  return (
+    <button type="button" className={styles.subscribe}>
+      Subscribe now
+    </button>
+  );
+}
