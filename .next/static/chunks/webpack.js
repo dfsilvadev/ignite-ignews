13a6ccb92e70f4c9d45f1a43d8c7136f95211108
Daniel Silva <dani3l.dxp@gmail.com>
2021-05-24 18:05:53 -0300

    feat: finalizando a estilização da page home

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "802ca1b2984b4ef46361"; }
+/******/ 		__webpack_require__.h = function() { return "87f16199501c73c10b5c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscriberButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
@@ -19,6 +20,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscriberButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
