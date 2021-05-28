0b8d3873600376f1e18cb499e80966eed45ad197
Daniel Silva <dani3l.dxp@gmail.com>
2021-05-28 17:45:06 -0300

    feat: criando webhooks para ouvir eventos do stripe e salvar os dados do evento no fauna

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9bb52b537eebd34c8a66"; }
+/******/ 		__webpack_require__.h = function() { return "dd7c62689622cca2a1cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
