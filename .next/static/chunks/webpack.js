61288fe40cc0e3e9d105a751621473dac74b7f2a
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-24 09:42:54 -0300

    feat: configurando o jest e o testing-library

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1eed559022b52358928"; }
+/******/ 		__webpack_require__.h = function() { return "01d77a066a3c541c0ae2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
