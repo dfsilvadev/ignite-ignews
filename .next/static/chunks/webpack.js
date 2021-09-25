afa0013703aa4132809622c1936fec224dd687eb
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-25 11:36:41 -0300

    feat: testando o component Header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "56c08ac226e080f487fc"; }
+/******/ 		__webpack_require__.h = function() { return "7357d9ce518c213c414e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
